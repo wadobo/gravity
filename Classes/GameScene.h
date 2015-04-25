24ca05744ed77825ba4013dde5fa2772ba2ee88e
Daniel Garcia Moreno <danigm@wadobo.com>
2015-04-25 12:42:44 +0200

    Added gpl licence

--- Classes/GameScene.h
+++ Classes/GameScene.h
@@ -1,3 +1,22 @@
+/**
+ * Copyright (C) 2015 Daniel Garcia <danigm@wadobo.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ **/
+
+
 #ifndef __GAME_SCENE_H__
 #define __GAME_SCENE_H__
 
@@ -10,13 +29,8 @@ namespace gravity {
 class GameScene : public cocos2d::Layer
 {
 public:
-    // there's no 'id' in cpp, so we recommend returning the class instance pointer
     static cocos2d::Scene* createScene();
-
-    // Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
     virtual bool init();
-
-    // implement the "static create()" method manually
     CREATE_FUNC(GameScene);
 
 private:
